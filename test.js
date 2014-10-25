9f4c5cb6ec2f33d2b3c9033c13c4c907c5dcc0d8
avil13 <13.avil@gmail.com>
2014-10-25 18:44:35 +0400

    test

--- test.js
+++ test.js
@@ -1,82 +1,95 @@
-/*
-var VK = require('VK');
+var blessed = require('blessed');
+// Create a screen object.
+var screen = blessed.screen();
+var settings = require('screen_settings')(screen);
+
+var ScreenBlocks = {};
+
+//  -------------
+ScreenBlocks.FriendList = blessed.List(settings.FriendList);
+
+ScreenBlocks.messages = blessed.Box(settings.messages);
+
+// поле ввода
+ScreenBlocks.txt = blessed.Textarea(settings.txt);
+
+ScreenBlocks.box = blessed.box(settings.box);
+
+ScreenBlocks.nav = blessed.box(settings.nav);
+
+screen.render();
+
+var Actions = require('Actions')(ScreenBlocks);
 
 
-var app = function() {
-    VK.request('feed.get', {}, function(data) {
-        console.log('return: ', data);
-        //        process.exit();
-    });
-};
-*/
-
-
-// VK.checkToken(app);
-
-
-
-// отобразить список друзей
-// this.friends = function() {
-
-// console.log(ScreenBlocks);
-
-// vk.request('friends.get', {
-//     fields: 'uid,first_name,last_name',
-//     count: 2,
-//     order: 'hints'
-// }, function(data) {
-//     //
-//     console.log( data );
-// });
-
-// };
-
-////
-
-
-/*
-
-FriendList.on('select', function(index) {
-    // получение id mid текущей беседы, и запись этого значения в переменную
-    var id = Actions.getID(index.content);
-    message_id = id;
-
-    // отправляем запрос на соединение и получение последних сообщений
-    if (id) {
-        Actions.getHistory(id, msg, screen);
-    }
+screen.key(['t', 'T', 'е', 'Е'], function(ch, key) {
+    ScreenBlocks.txt.focus();
 });
-*/
 
-// setInterval(function() {
+screen.key(['f', 'F', 'а', 'А'], function(ch, key) {
+    ScreenBlocks.FriendList.focus();
+});
 
-//     Actions.getDialogs(FriendList, screen, msg);
-
-//    память
-// var m = process.memoryUsage().heapUsed;
-// box.setContent( m+''  );
-// nav.setContent( (Math.random() * (99 - 1) + 1)+'');
-// }, 3000);
-
-// setInterval(function() {
-//     Actions.friends();
-//     // if (message_id)
-//         // Actions.getHistory(message_id, msg, screen);
-// }, 2660);
+screen.key(['r', 'R', 'к', 'К'], function(ch, key) {
+    ScreenBlocks.messages.focus();
+});
 
 
-//////
-
-/*
-screen.key('tab', function(ch, key) {
-    if (!key.shift) {
-        screen.focusNext();
-    } else {
-        screen.focusPrev();
-    }
+// ******** focuses ********
+ScreenBlocks.FriendList.on('focus', function() {
+    ScreenBlocks.box.setContent("{bold}Active:{/bold} Friend list [F]");
     screen.render();
 });
-*/
 
-// *    ****************
-// screen.append(txt);
+ScreenBlocks.txt.on('focus', function() {
+    ScreenBlocks.box.setContent("{bold}Active:{/bold} Text write [T]");
+    screen.render();
+});
+
+ScreenBlocks.messages.on('focus', function() {
+    Actions.messageAsReadest();
+    Actions.getHistory();
+    ScreenBlocks.box.setContent("{bold}Active:{/bold} Read message [R]");
+    screen.render();
+});
+// ********
+
+
+/**
+ * выход
+ */
+ScreenBlocks.FriendList.key(['escape'], function(ch, key) {
+    return process.exit(0);
+});
+
+/**
+ * работа с полем чтрения сообщений
+ */
+ScreenBlocks.messages.key(['escape'], function(ch, key) {
+    ScreenBlocks.FriendList.focus();
+});
+
+/**
+ * работа с полем ввода
+ */
+ScreenBlocks.txt.key(['escape'], function(ch, key) {
+    ScreenBlocks.FriendList.focus();
+});
+
+
+function rand(Arr) {
+    var min = 0;
+    var max = Arr.length - 1;
+
+    return Arr[Math.floor(Math.random() * (max - min + 1)) + min];
+}
+
+Names = ['Джон', 'Стив', 'Билл', 'Денис', 'Анна', 'Елена', 'Галина'];
+SurNames = ['Сильвер', 'Джобс', 'Гейтс', 'Балмер', 'Родман', 'Джексон', 'Веллер'];
+
+/// ====
+
+for (var i = 0; i < 30; i++) {
+    ScreenBlocks.FriendList.add('{bold}' + rand(Names) + ' ' + rand(SurNames) + ' {/bold} » text of your mesage');
+}
+screen.render();
